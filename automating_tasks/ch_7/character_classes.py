efd5110b2c1ae487efd407feb09598f44351bd76
neeraj1909 <neeraj1909@gmail.com>
2021-04-30 13:10:40 +0530

    character classes

---
+++ automating_tasks/ch_7/character_classes.py
@@ -0,0 +1,6 @@
+import re
+
+
+xmas_regex = re.compile(r'\d+\s\w+')
+mo = xmas_regex.findall('12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, swans, 6 geese, 5 rings, 4 birds, 3 hens, 2 doves, 1 partridge')
+print(mo)
