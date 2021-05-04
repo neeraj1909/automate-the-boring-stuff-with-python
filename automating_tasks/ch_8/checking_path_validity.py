d28bbf798b3c97a4b0cd3752a7aa6be65629fd9e
neeraj1909 <neeraj1909@gmail.com>
2021-05-04 22:45:21 +0530

    checking path validity

---
+++ automating_tasks/ch_8/checking_path_validity.py
@@ -0,0 +1,9 @@
+import os
+
+
+print(os.path.exists('/home/neeraj/Code/automate-the-boring-stuff-with-python/automating_tasks/ch_8'))
+print(os.path.exists('/home/neeraj/Code/automate-the-boring-stuff-with-python/automating_tasks/ch_8/checking_validity.py'))
+print(os.path.isdir('/home/neeraj/Code/automate-the-boring-stuff-with-python/automating_tasks/ch_10'))
+print(os.path.isdir('/home/neeraj/Code/automate-the-boring-stuff-with-python/automating_tasks/ch_7'))
+print(os.path.isfile('/home/neeraj/Code/automate-the-boring-stuff-with-python/automating_tasks/ch_8/checking_path_validity.py'))
+print(os.path.isfile('/home/neeraj/Code/automate-the-boring-stuff-with-python/automating_tasks/ch_8/tmp.py'))
